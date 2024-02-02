41da218d2ff560594484a3b7846bcde048099557
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-02-02 00:49:33 -0500

    Edit Los Angeles time zone to correct date

--- javascript/index.js
+++ javascript/index.js
@@ -2,11 +2,12 @@ function updateTime() {
 // Los Angeles
 let losAngelesElement = document.querySelector("#los-angeles");
 if (losAngelesElement) {
+
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = moment().format("dddd, MMMM Do, YYYY");
+losAngelesDateElement.innerHTML = losAngelesTime.format("dddd, MMMM Do, YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
